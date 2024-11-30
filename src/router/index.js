00ab4299d7d7864b40f65df6a99fe2ad5817d6e8
Chea Oudompanhariddh <cheaodompanhariddh.com>
2024-11-30 13:53:43 +0700

    Ex1 & Ex2:Configure Router For HomeView

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router'
-//import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '@/views/HomeView.vue';
+
+const routes = [
+  { path: '/', component: HomeView }, 
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-  
-    },
-    {
-    //  path: '/about',
-     // name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-     // component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  history: createWebHistory(),
+  routes, 
+});
 
-export default router
+export default router;
